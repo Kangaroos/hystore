021a27d402c596c1bb144b05cb8445e0d01f666c
xyezir <xyezir@gmail.com>
2016-01-27 07:48:19 +0800

    add new migrate

--- db/migrations/m20160124_132105_CreateActivityPrizeVertex.js
+++ db/migrations/m20160124_132105_CreateActivityPrizeVertex.js
@@ -3,9 +3,22 @@ exports.name = "CreateActivityPrizeVertex";
 
 exports.up = function (db) {
   // @todo implementation
+  return db.class.create('ActivityPrize', 'V')
+  .then(function (c) {
+    return Promise.all([
+      c.property.create({name: 'name', type: 'String', mandatory: true}),
+      c.property.create({name: 'type', type: 'String'}),
+      c.property.create({name: 'number', type: 'Integer'}),
+      c.property.create({name: 'valid_date', type: 'DateTime'}),
+      c.property.create({name: 'description', type: 'String'}),
+      c.property.create({name: 'created_at', type: 'DateTime', default: 'sysdate()'}),
+      c.property.create({name: 'updated_at', type: 'DateTime', default: 'sysdate()'})
+    ]);
+  })
 };
 
 exports.down = function (db) {
   // @todo implementation
+  return db.class.drop('ActivityPrize');
 };
 
